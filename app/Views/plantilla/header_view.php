a3bec51142f5682e86ac597deb663eb2e3bd7fae
Diego N. Cabrera <87397817+1Hagi@users.noreply.github.com>
2025-04-23 17:50:48 -0300

    creada plantilla de formulario de registro completa en vista header.

--- app/Views/plantilla/header_view.php
+++ app/Views/plantilla/header_view.php
@@ -37,21 +37,79 @@
                 </div>
                 <div class="modal-body">
                     <form id="formRegistro">
-                        <div class="mb-3">
-                            <label for="emailRegistro" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="emailRegistro" required>
+                        <div class="col-12">
+                            <div
+                                style="font-size: 16px ;letter-spacing: 0.2rem; inter">
+                                DATOS DE USUARIO
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="emailRegistro" class="form-label formulario">Email</label>
+                                    <input type="email" class="form-control" id="emailRegistro" required>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="nombreRegistro" class="form-label formulario">Nombre de Usuario</label>
+                                    <input type="text" class="form-control" id="nombreRegistro" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="passRegistro" class="form-label formulario">Contraseña</label>
+                                    <input type="password" class="form-control" id="passRegistro" required>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="passConfRegistro" class="form-label formulario">Repetir contraseña</label>
+                                    <input type="password" class="form-control" id="passConfRegistro" required>
+                                </div>
+                            </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="emailRegistro" class="form-label">Nombre de Usuario</label>
-                            <input type="email" class="form-control" id="emailRegistro" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="passRegistro" class="form-label">Contraseña</label>
-                            <input type="password" class="form-control" id="passRegistro" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="emailRegistro" class="form-label">Repetir contraseña</label>
-                            <input type="email" class="form-control" id="emailRegistro" required>
+                        <div class="col-12">
+                            <div
+                                style="font-size: 16px ;letter-spacing: 0.2rem; inter">
+                                DATOS DE ENVIO
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="dniRegistro" class="form-label formulario">DNI</label>
+                                    <input type="number" class="form-control" id="dniRegistro" required>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="fechaNacRegistro" class="form-label formulario">Fecha de Nacimiento</label>
+                                    <input type="date" class="form-control" id="fechaNacRegistro" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="direccionRegistro" class="form-label formulario">Dirección</label>
+                                    <input type="text" class="form-control" id="direccionRegistro" required>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="provRegistro" class="form-label formulario">Provicia/Estado</label>
+                                    <input type="text" class="form-control" id="provRegistro" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col">
+                                    <label for="paisRegistro" class="form-label formulario">Pais</label>
+                                    <input type="text" class="form-control" id="paisRegistro" required>
+                                </div>
+                                <div class="mb-3 col">
+                                    <label for="cpRegistro" class="form-label formulario">Código Postal</label>
+                                    <input type="number" class="form-control" id="cpRegistro" required>
+                                </div>
+                            </div>
+                            <div class="mb-3 col">
+                                <input type="checkbox" class="" id="terminosRegistro" required>
+                                <label for="terminoRegistro" class="form-label formulario" style="margin-left:5px; margin-top: 10px;font-size: 17px !important">
+                                    Aceptar <a href="<?= base_url('terminos') ?>">Terminos y Condiciones de Uso.</a>
+                                </label>
+                            </div>
+                            <div class="mb-3 col">
+                                <input type="checkbox" class="" id="newsRegistro">
+                                <label for="newsRegistro" class="form-label formulario" style="margin-left:5px;font-size: 17px !important">
+                                    Suscribete a nuestro newsletter recibir las mejores ofertas.</a>
+                                </label>
+                            </div>
                         </div>
                         <button type="submit" class="btn btn-primary w-100">Registrarse</button>
                     </form>
