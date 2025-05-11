48b256ac6f03f042008177c5bf1cbf747b5a00a5
Diego N. Cabrera <87397817+1Hagi@users.noreply.github.com>
2025-05-11 17:01:02 -0300

    Se agregaron ejemplo de filtros y sección de cards de productos

--- app/Views/contenido/productos_view.php
+++ app/Views/contenido/productos_view.php
@@ -1,3 +1,162 @@
-<h1 class="d-flex align-items-center justify-content-center py-4">
-    PROBANDO PÁGINA: <?= $titulo ?>
-</h1>
+<main class="container-fluid p-0">
+
+  <div class="container-fluid d-flex align-items-center justify-content-center nav-categorias">
+    <div class="" role="group" aria-label="Filtro">
+      <input type="radio" class="btn-check" name="filtro" id="hombre" autocomplete="off">
+      <label class="btn btn-outline-info" for="hombre">Hombre</label>
+
+      <input type="radio" class="btn-check" name="filtro" id="mujer" autocomplete="off">
+      <label class="btn btn-outline-info" for="mujer">Mujer</label>
+
+      <input type="radio" class="btn-check" name="filtro" id="niños" autocomplete="off">
+      <label class="btn btn-outline-info" for="niños">Niños</label>
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center justify-content-center" style="height: 5rem; background-color: rgb(71, 88, 105)">
+    <div class="" role="group" aria-label="Filtro">
+
+      <div class="btn p-0" for="hombre">
+        <img class="img-fluid" src="./assets/img/logo_verano.png" style="height: 5rem;" />
+      </div>
+
+      <div class="btn p-0" for="hombre">
+        <img class="img-fluid" src="./assets/img/logo_invierno.png" style="height: 5rem;" />
+      </div>
+
+      <div class="btn p-0" for="hombre" style="position: relative;">
+        <img class="img-fluid" src="./assets/img/logo_otoño.png" style="height: 5rem;" />
+      </div>
+
+    </div>
+  </div>
+
+  <hr style="border: 0; height: 2px; background-image: linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,0.75), rgba(255,255,255,0));">
+
+  <div class="container-lg d-flex flex-wrap justify-content-center gap-3 p-3">
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/oferta1.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Zapatillas Nike Air Max Hombre
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 199,99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/otoño1.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Campera Running Puma Mesh
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 199.99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/oferta2.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Zapatillas Running Puma
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 89.99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/otoño3.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Remera Training Hang Loose
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 119.99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/oferta3.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Zapatillas Puma Park Lifestyle
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 119.99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/otoño2.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Buzo Training Kion Skill
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 89.99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/oferta1.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Zapatillas Nike Air Max Hombre
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 199,99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card p-2 bg-transparent border-secondary" style="width: 14rem; height: 24rem;">
+      <img src="./assets/img/otoño1.jpg" class="img-fluid" alt="..." style="height: 75%; object-fit: cover;">
+      <div class="card-body bg-transparent p-0 pt-1" style="height: 25%">
+        <h5 class="card-title text-white text-center d-flex align-items-center justify-content-center" style="height: 3rem; overflow: hidden; text-overflow: ellipsis; font-size: 1rem;">
+          Campera Running Puma Mesh
+        </h5>
+        <div class="card-text d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-white fs-6"><strong> ARS$ 199.99 </strong></p>
+          <button type="button" class="btn btn-outline-secondary rounded-pill p-0" data-bs-toggle="button" style="width: 3.5rem; height: 2rem;">
+            <img class="img-fluid" src="./assets/img/buy-icon.png" alt="buy" style="height: 90%; object-fit: cover;"/>
+          </button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <hr style="border: 0; height: 2px; background-image: linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,0.75), rgba(255,255,255,0));">
+
+</main>
